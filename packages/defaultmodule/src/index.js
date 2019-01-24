0e8772ac22383e5e45b4071a53f6d08bce93d860
xiaolei <lei.xiao@ifchange.com>
2019-01-24 23:17:53 +0800

    test: default in react

--- packages/defaultmodule/src/index.js
+++ packages/defaultmodule/src/index.js
@@ -2,6 +2,6 @@ export default async function delayPromise(str, ms = 0) {
   const promise = new Promise(resolve => {
     setTimeout(() => resolve(str), ms)
   })
-  console.log('sss')
+  console.log('sssxxxxxx')
   return await promise
 }
