10c3408d61067bc7628d8c2e3b070875085c2362
xiaolei <lei.xiao@ifchange.com>
2019-01-24 23:38:02 +0800

    lerna publish --canary --preid next

--- packages/defaultmodule/src/index.js
+++ packages/defaultmodule/src/index.js
@@ -2,6 +2,6 @@ export default async function delayPromise(str, ms = 0) {
   const promise = new Promise(resolve => {
     setTimeout(() => resolve(str), ms)
   })
-  console.log('sssxxxxxxdssss--lerna publish --canary --preid')
+  console.log('lerna publish --canary --preid next')
   return await promise
 }
