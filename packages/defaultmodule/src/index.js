cb4348f3835ce51ccc42da7653ac70cb24946cd4
xiaolei <lei.xiao@ifchange.com>
2019-01-25 18:05:06 +0800

    lerna changed

--- packages/defaultmodule/src/index.js
+++ packages/defaultmodule/src/index.js
@@ -1,7 +1,7 @@
 export default async function delayPromise(str, ms = 0) {
-  const promise = new Promise(resolve => {
-    setTimeout(() => resolve(str), ms)
-  })
-  console.log('sssxxxxxxdssss--lersna publish --canary --preid')
-  return await promise
+	const promise = new Promise(resolve => {
+		setTimeout(() => resolve(str), ms)
+	})
+	console.log('sssxxxxxxdssss--lerna changed')
+	return await promise
 }
