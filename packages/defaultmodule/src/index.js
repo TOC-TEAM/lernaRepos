351998890d9d1f7631e96b081469a955a24d116a
xiaolei <lei.xiao@ifchange.com>
2019-03-19 15:07:23 +0800

    chore: add third package

--- packages/defaultmodule/src/index.js
+++ packages/defaultmodule/src/index.js
@@ -2,6 +2,6 @@ export default async function delayPromise(str, ms = 0) {
 	const promise = new Promise(resolve => {
 		setTimeout(() => resolve(str), ms)
 	})
-	console.log('sssxxxxxxdssss--lerna diff')
+	console.log('sssxxxxxxdssss--lerna diffssss')
 	return await promise
 }
